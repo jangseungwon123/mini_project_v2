1c5997fd95087d000e43e19a94824fbcafa935e3
jang seungwon <rhanwnf97@naver.com>
2025-07-31 19:29:57 +0900

    first commit

---
+++ src/main/java/com/tenco/jobpotal/JobpotalApplication.java
@@ -0,0 +1,13 @@
+package com.tenco.jobpotal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobpotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobpotalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenco/jobpotal/JobpotalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenco.jobpotal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpotalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
