5911949c0408344f4b7ca66bd43807775389ebb9
jibaek <tngus000777@gmail.com>
2025-08-01 15:59:38 +0900

    ResumeRequest,ResumeResponse 작성완료

--- src/main/java/com/tenco/jobpotal/resume/ResumeRestController.java
+++ src/main/java/com/tenco/jobpotal/resume/ResumeRestController.java
@@ -1,12 +1,28 @@
 package com.tenco.jobpotal.resume;
 
+import com.tenco.jobpotal._core.common.ApiUtil;
+import com.tenco.jobpotal.user.LoginUser;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class ResumeRestController {
 
+    @PostMapping("/api/resumes")
+    public ResponseEntity<?> save(@Valid @RequestBody ResumeRequest.SaveDTO saveDTO,
+                                  Errors errors,
+                                  @RequestAttribute("sessionUser")LoginUser loginUser) {
 
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ApiUtil<>());
+    }
 
 }
--- src/main/java/com/tenco/jobpotal/resume/ResumeService.java
+++ src/main/java/com/tenco/jobpotal/resume/ResumeService.java
@@ -1,13 +1,27 @@
 package com.tenco.jobpotal.resume;
 
 
+import com.tenco.jobpotal.user.LoginUser;
+import com.tenco.jobpotal.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class ResumeService {
 
+    private final ResumeJpaRepository resumeJpaRepository;
 
-
+    @Transactional
+    public ResumeResponse.SaveDTO save(ResumeRequest.SaveDTO saveDTO, LoginUser loginUser){
+        User user = User.builder()
+                .userId(loginUser.getId())
+                .useName(loginUser.getName())
+                .userNickname(loginUser.getUserNickName())
+                .build();
+        Resume resume = saveDTO.toEntity(user);
+        Resume saveResume = resumeJpaRepository.save(resume);
+        return new ResumeResponse.SaveDTO(saveResume);
+    }
 }
