23320bdbf9898fc6c77ddc498213ddb34d75628f
jibaek <tngus000777@gmail.com>
2025-08-04 16:26:38 +0900

    FAQ 기본 파일 생성 및 엔티티 설계

---
+++ src/main/java/com/tenco/jobpotal/faq/FAQInfo.java
@@ -0,0 +1,34 @@
+package com.tenco.jobpotal.faq;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "faq_info")
+public class FAQInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long faqId;
+
+    private String title;
+    private String content;
+
+    private String instId;
+    @CreationTimestamp
+    private Timestamp instDate;
+
+    public FAQInfo(Long faqId, String title, String content, String instId, Timestamp instDate) {
+        this.faqId = faqId;
+        this.title = title;
+        this.content = content;
+        this.instId = instId;
+        this.instDate = instDate;
+    }
+}
