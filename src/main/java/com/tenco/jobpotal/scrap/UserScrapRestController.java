2d18d3b61610248a5cbcc409418077b4f6a49cbe
suuu1021 <whs5758@naver.com>
2025-08-05 12:36:13 +0900

    userscrap 매핑경로 수정

--- src/main/java/com/tenco/jobpotal/scrap/UserScrapRestController.java
+++ src/main/java/com/tenco/jobpotal/scrap/UserScrapRestController.java
@@ -19,21 +19,21 @@ public class UserScrapRestController {
     private final UserScrapService userScrapService;
 
 
-    @PostMapping("/user_sub")
+    @PostMapping("/user_scrap")
     public ResponseEntity<?> save(@Valid @RequestBody UserScrapRequest.SaveDTO saveDTO, Errors errors,
                                   @RequestAttribute(Define.LOGIN_USER) LoginUser loginUser) {
         UserScrapResponse.SaveDTO responseDTO = userScrapService.save(saveDTO, loginUser);
         return ResponseEntity.ok(new ApiUtil<>(responseDTO));
     }
 
-    @GetMapping("/user_sub/list")
+    @GetMapping("/user_scrap/list")
     public ResponseEntity<?> list(@RequestAttribute(Define.LOGIN_USER) LoginUser loginUser) {
       List<UserScrapResponse.ScrapListDTO> userSubList = userScrapService.findAllByUserAndJobPostId(loginUser.getId());
       return ResponseEntity.ok(new ApiUtil<>(userSubList));
     }
 
 
-    @PostMapping("/user_sub/{id}/delete")
+    @PostMapping("/user_scrap/{id}/delete")
     public ResponseEntity<ApiUtil<String>> delete(@PathVariable(name = "id") Long id,
                                                   @RequestAttribute(Define.LOGIN_USER) LoginUser loginUser) {
         userScrapService.deleteById(id, loginUser);
