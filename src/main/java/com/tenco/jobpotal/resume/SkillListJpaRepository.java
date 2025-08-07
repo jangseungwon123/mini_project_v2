4a522c5593675db3b2b4758bce2799358106cc89
TrainingScav <dnslwk123@naver.com>
2025-08-07 17:31:00 +0900

    skillListJpaRepository 추가

---
+++ src/main/java/com/tenco/jobpotal/resume/SkillListJpaRepository.java
@@ -0,0 +1,16 @@
+package com.tenco.jobpotal.resume;
+
+
+import com.tenco.jobpotal.company.CompInfo;
+import com.tenco.jobpotal.skill.SkillList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface SkillListJpaRepository extends JpaRepository<SkillList, Long> {
+
+    @Query("SELECT sl FROM SkillList sl WHERE sl.skillId = :skillId")
+    Optional<SkillList> findBySkillId(@Param("skillId") Long skillId);
+}
