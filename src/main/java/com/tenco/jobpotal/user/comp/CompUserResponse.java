e18bfb9b78f50210c2c240f4d9d4cfb04ed13ea8
jang seungwon <rhanwnf97@naver.com>
2025-08-11 20:50:00 +0900

    rating 기능 수정중

--- src/main/java/com/tenco/jobpotal/user/comp/CompUserResponse.java
+++ src/main/java/com/tenco/jobpotal/user/comp/CompUserResponse.java
@@ -28,7 +28,7 @@ public class CompUserResponse {
 			this.compUserEmail = compUser.getCompUserEmail();
 			this.compUserNickname = compUser.getCompUserNickname();
 			this.compRegNumber = compUser.getCompRegNumber();
-		//	this.createdAt = compUser.getCreatedAt().toString();
+			this.createdAt = compUser.getCreatedAt().toString();
 		}
 	}
 
